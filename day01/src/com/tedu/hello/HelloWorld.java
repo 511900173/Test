bea30ec1113e0a3433d9a123493b5eac5f978991
Administrator <Administrator@PC-20190530VXBI>
2019-10-17 15:11:41 +0800

    a

---
+++ day01/src/com/tedu/hello/HelloWorld.java
@@ -0,0 +1,49 @@
+package com.tedu.hello;
+/*定义一个包
+ * package 是Java的关键字，用来定义包的关键字；
+ * com.tedu.hello 包名   1全小写   2公司的域名倒着写
+ *   ; Java中的语句结束符
+ */
+
+
+/*
+ * 定义一个类
+ * public 是关键字，作为类的修饰符，公共的  （修饰范围  最大的）
+ * class  关键字  ， 用来定义类的关键字
+ * HelloWorld  类名  -- 符合驼峰命名法
+ * ｛java代码｝
+ */
+
+public class HelloWorld {
+	/*
+	 * 定义一个主函数/主方法，作为程序的入口
+	 * public 关键字，作为主方法的修饰符，公共的
+	 * static 关键字，说明 主方法是静态的
+	 * void   关键字，主方法是没有返回值的
+	 * main   方法名，
+	 * ()   方法的标志
+	 *    String[]   args  方法的参数
+	 *    参数的类型         参数名
+	 * {  Java代码   }
+	 */
+	public static void main(String[] args) {
+		/*
+		 * 向控制台输出制定的内容
+		 * system   系统 ，让程序发出系统指令
+		 *   .      调用功能
+		 *  out     让系统发出一个输出的动作
+		 * println  打印输出的内容并换行
+		 *   ()     方法的标志
+		 *  123             方法的参数，指定输出的内容 
+		 */
+
+
+		System.out.println(123);
+		System.out.println(456);
+		
+		int Ste;
+		int cLanguageScor;
+
+	}
+
+}
